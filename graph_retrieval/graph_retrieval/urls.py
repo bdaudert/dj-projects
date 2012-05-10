21eb608458af1037fa0839367b626dccda4a248a
Britta Daudert <bdaudert@dri.edu>
2012-05-10 16:58:25 -0700

    created admin pages, populated db and views

--- graph_retrieval/graph_retrieval/urls.py
+++ graph_retrieval/graph_retrieval/urls.py
@@ -1,17 +1,20 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
+    url(r'^plots_7d/$', 'plots_7d.views.index', name='index'),
+    url(r'^plots_7d/results/$', 'plots_7d.views.results', name='results'),
+    url(r'^plots_7d/(?P<state_name>[A-Z][A-Z]|\d+)/$', 'plots_7d.views.stations', name='stations'),
     # url(r'^$', 'graph_retrieval.views.home', name='home'),
     # url(r'^graph_retrieval/', include('graph_retrieval.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
