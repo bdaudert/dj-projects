469bf396d5c3848292717d54e415862ac34d9bdd
Britta Daudert <bdaudert@dri.edu>
2012-05-15 14:46:53 -0700

    Added request methods to my_meta.views

--- my_acis/my_acis/urls.py
+++ my_acis/my_acis/urls.py
@@ -10,7 +10,14 @@ urlpatterns = patterns('',
     # url(r'^my_acis/', include('my_acis.foo.urls')),
     (r'^$', 'my_acis.views.home_view'),
     (r'^my_meta$', 'my_meta.views.home_view'),
-
+    (r'^my_meta/by_state/$', 'my_meta.views.by_state'),
+    (r'^my_meta/by_name/$', 'my_meta.views.by_name'),
+    (r'^my_meta/by_id/$', 'my_meta.views.by_id'),
+    (r'^my_meta/by_location/$', 'my_meta.views.by_location'),
+    (r'^my_meta/station_detail/$', 'my_meta.views.station_detail'),
+    (r'^my_meta/maintenance/(?P<ucan_id>\d+)/$', 'my_meta.views.station_maintenance'),
+    (r'^my_meta/physical/(?P<ucan_id>\d+)/$', 'my_meta.views.station_physical'),
+    (r'^my_meta/equipment/(?P<ucan_id>\d+)/$', 'my_meta.views.station_equipment'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
--- my_acis/my_meta/views.py
+++ my_acis/my_meta/views.py
@@ -20,3 +20,179 @@ def home_view(request):
         'title': "Metadata Home",
     }
     return render_to_response('my_meta/home.html', context, context_instance=RequestContext(request))
+
+def by_state(request):
+    state_key = request.GET.get('state_key', None)
+    if state_key is None:
+        stations = []
+    else:
+        stations = Station.objects.filter(state=int(state_key)).order_by('station_best_name')
+
+    context = {
+        'stations': stations,
+        'title': "Stations by State",
+    }
+    return render_to_response('my_meta/results.html', context, context_instance=RequestContext(request))
+def by_name(request):
+    q = request.GET.get('q', '')
+    if not q:
+        stations = []
+    else:
+        stations = Station.objects.filter(station_best_name__icontains=q).order_by('station_best_name')
+
+    context = {
+        'stations': stations,
+        'title': "Station Name Search Results",
+    }
+    return render_to_response('my_meta/results.html', context, context_instance=RequestContext(request))
+
+def by_id(request):
+    q = request.GET.get('q', '')
+    if not q:
+        stations = []
+    else:
+        stations = StationNetwork.objects.filter(network_station_id__icontains=q)
+        ucan_ids = [s.ucan_station_id for s in stations]
+        stations = Station.objects.filter(ucan_station_id__in=ucan_ids)
+    context = {
+        'stations': stations,
+        'title': "Station ID Search Results",
+    }
+    return render_to_response('my_meta/results.html', context, context_instance=RequestContext(request))
+
+def by_location(request):
+    lat = request.GET.get('lat', None)
+    lon = request.GET.get('lon', None)
+    delta = .51
+    if not lat or not lon:
+        stations = []
+    else:
+        lat = float(lat)
+        lon = float(lon)
+        stations = StationLocation.objects.filter(latitude__lte=lat+delta, latitude__gte=lat-delta,
+                                                    longitude__lte=lon+delta, longitude__gte=lon-delta,
+                                                    history_flag__icontains='L'
+                                                )
+        ucan_ids = [s.ucan_station_id for s in stations]
+        stations = Station.objects.filter(ucan_station_id__in=ucan_ids)
+    context = {
+        'stations': stations,
+        'title': "Station Location Search Results",
+    }
+    return render_to_response('my_meta/results.html', context, context_instance=RequestContext(request))
+
+
+def station_detail(request):
+    ucan_id = request.GET.get('ucan_id', None)
+    context = {
+        'title': "Station Detail",
+    }
+
+    if ucan_id:
+        ucan_id = int(ucan_id)
+        context['ucan_station_id'] = ucan_id
+        context['station'] = set_as_table(Station.objects.get(pk=ucan_id))
+        context['station_location'] = set_as_table(StationLocation.objects.filter(ucan_station_id=ucan_id))
+        context['station_maintenance'] = set_as_table(StationMaintenance.objects.filter(ucan_station_id=ucan_id))
+        context['station_equipment'] = set_as_table(StationEquipment.objects.filter(ucan_station_id=ucan_id))
+        context['station_physical'] = set_as_table(StationPhysical.objects.filter(ucan_station_id=ucan_id))
+    return render_to_response('my_meta/station_detail.html', context, context_instance=RequestContext(request))
+
+#@login_required
+def station_maintenance(request, ucan_id):
+    ucan_id = int(ucan_id)
+    context = {
+        'title': "Station Maintenance",
+        'ucan_id': ucan_id,
+    }
+    if request.POST:
+        form = StationMaintenanceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            context['saved'] = "Information saved."
+    else:
+        form = StationMaintenanceForm(initial={'ucan_station_id': ucan_id})
+
+    context['form'] = form
+    context['station_maintenance'] = set_as_table(StationMaintenance.objects.filter(ucan_station_id=ucan_id))
+    return render_to_response('my_meta/station_maintenance.html', context, context_instance=RequestContext(request))
+
+#@login_required
+def station_physical(request, ucan_id):
+    ucan_id = int(ucan_id)
+    context = {
+        'title': "Station Physical",
+        'ucan_id': ucan_id,
+    }
+    if request.POST:
+        form = StationPhysicalForm(request.POST)
+        if form.is_valid():
+            form.save()
+            context['saved'] = "Information saved."
+    else:
+        form = StationPhysicalForm(initial={'ucan_station_id': ucan_id})
+
+    context['form'] = form
+    context['station_physical'] = set_as_table(StationPhysical.objects.filter(ucan_station_id=ucan_id))
+    return render_to_response('my_meta/station_physical.html', context, context_instance=RequestContext(request))
+
+#@login_required
+def station_equipment(request, ucan_id):
+    ucan_id = int(ucan_id)
+    context = {
+        'title': "Station Equipment",
+        'ucan_id': ucan_id,
+    }
+    if request.POST:
+        form = StationEquipmentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            context['saved'] = "Information saved."
+    else:
+        form = StationEquipmentForm(initial={'ucan_station_id': ucan_id})
+
+    context['form'] = form
+    context['station_equipment'] = set_as_table(StationEquipment.objects.filter(ucan_station_id=ucan_id))
+    return render_to_response('my_meta/station_equipment.html', context, context_instance=RequestContext(request))
+
+#
+# Utility functions
+#
+def set_as_table(qs):
+    """
+    Return a query set as an HTML table without <table> tags.
+    """
+    first = True
+    headers = []
+    rows = []
+
+    if isinstance(qs, QuerySet):
+        for i in qs:
+            if first:
+                first = False
+                headers.append("<tr>")
+                for f in i._meta.fields:
+                    headers.append("<th>%s</th>" % f.name)
+                headers.append("</tr>")
+            rows.append("<tr>")
+            for f in i._meta.fields:
+                rows.append("<td>%s</td>" % break_text(getattr(i, f.name)))
+            rows.append("</tr>")
+    elif hasattr(qs, '_meta'):
+        headers.append("<tr>")
+        rows.append("<tr>")
+        for f in qs._meta.fields:
+            headers.append("<th>%s</th>" % f.name)
+            rows.append("<td>%s</td>" % break_text(getattr(qs, f.name)))
+        headers.append("</tr>")
+        rows.append("</tr>")
+    else:
+        raise
+    return "\n".join(headers + rows)
+
+def break_text(s,num=60,sep="<br />"):
+    """ Return string s with separator every num words."""
+    from django.utils.text import wrap
+    from django.utils.safestring import mark_safe
+    return wrap(mark_safe(s), num).replace('\n', sep)
+
