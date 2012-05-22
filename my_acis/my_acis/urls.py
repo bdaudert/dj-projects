ecdda8622ff3469b2f3302ab88a5c01194cca6c1
Britta Daudert <bdaudert@dri.edu>
2012-05-22 12:06:55 -0700

    added search through common db tables

--- my_acis/my_acis/urls.py
+++ my_acis/my_acis/urls.py
@@ -31,8 +31,3 @@ if settings.DEBUG:
             'document_root': settings.MEDIA_ROOT,
         }),
    )
-    urlpatterns += patterns('',
-        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.STATIC_ROOT,
-        }),
-   )
