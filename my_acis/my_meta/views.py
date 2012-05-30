32b5d3dbef6648380d770d185f10b27daf9880c2
Britta Daudert <bdaudert@dri.edu>
2012-05-30 12:42:03 -0700

    cleaned up views

--- my_acis/my_meta/views.py
+++ my_acis/my_meta/views.py
@@ -1,5 +1,10 @@
 # Create your views here.
+#Python imports
 import datetime
+from collections import defaultdict
+import inspect
+
+#django Imports
 from django.template import RequestContext
 #from django.contrib.auth.models import User
 #from django.contrib.auth.decorators import login_required
@@ -9,20 +14,13 @@ from django.shortcuts import render_to_response
 from django.views.generic.list_detail import object_detail, object_list
 from django.db.models.query import QuerySet
 
-from my_meta.models import *
-from my_meta.forms import *
-
-#My  additions. FIX ME: get rid of from import *
-from collections import defaultdict
-import inspect
+#project/app imports
 import models
-import my_meta.forms
-from django.forms.models import modelformset_factory
+import my_meta.forms as mforms
 
 def home_view(request):
-#    return HttpResponse("Hello, you. You're at the my_acis/my_meta home page.")
     context = {
-        'state_choices': STATE_CHOICES,
+        'state_choices': mforms.STATE_CHOICES,
         'title': "Metadata Home",
     }
     return render_to_response('my_meta/home.html', context, context_instance=RequestContext(request))
@@ -32,7 +30,8 @@ def by_state(request):
     if state_key is None:
         stations = []
     else:
-        stations = Station.objects.filter(state=int(state_key)).order_by('station_best_name')
+        stations = models.Station.objects.filter(state=int(state_key)).order_by('station_best_name')
+
 
     context = {
         'stations': stations,
@@ -44,7 +43,7 @@ def by_name(request):
     if not q:
         stations = []
     else:
-        stations = Station.objects.filter(station_best_name__icontains=q).order_by('station_best_name')
+        stations = models.Station.objects.filter(station_best_name__icontains=q).order_by('station_best_name')
 
     context = {
         'stations': stations,
@@ -57,9 +56,9 @@ def by_id(request):
     if not q:
         stations = []
     else:
-        stations = StationNetwork.objects.filter(network_station_id__icontains=q)
+        stations = models.StationNetwork.objects.filter(network_station_id__icontains=q)
         ucan_ids = [s.ucan_station_id for s in stations]
-        stations = Station.objects.filter(ucan_station_id__in=ucan_ids)
+        stations = models.Station.objects.filter(ucan_station_id__in=ucan_ids)
     context = {
         'stations': stations,
         'title': "Station ID Search Results",
@@ -75,12 +74,12 @@ def by_location(request):
     else:
         lat = float(lat)
         lon = float(lon)
-        stations = StationLocation.objects.filter(latitude__lte=lat+delta, latitude__gte=lat-delta,
+        stations = models.StationLocation.objects.filter(latitude__lte=lat+delta, latitude__gte=lat-delta,
                                                     longitude__lte=lon+delta, longitude__gte=lon-delta,
                                                     history_flag__icontains='L'
                                                 )
         ucan_ids = [s.ucan_station_id for s in stations]
-        stations = Station.objects.filter(ucan_station_id__in=ucan_ids)
+        stations = models.Station.objects.filter(ucan_station_id__in=ucan_ids)
     context = {
         'stations': stations,
         'title': "Station Location Search Results",
@@ -96,11 +95,11 @@ def station_detail(request):
     if ucan_id:
         ucan_id = int(ucan_id)
         context['ucan_station_id'] = ucan_id
-        context['station'] = set_as_table(Station.objects.get(pk=ucan_id))
-        context['station_location'] = set_as_table(StationLocation.objects.filter(ucan_station_id=ucan_id))
-        context['station_maintenance'] = set_as_table(StationMaintenance.objects.filter(ucan_station_id=ucan_id))
-        context['station_equipment'] = set_as_table(StationEquipment.objects.filter(ucan_station_id=ucan_id))
-        context['station_physical'] = set_as_table(StationPhysical.objects.filter(ucan_station_id=ucan_id))
+        context['station'] = set_as_table(models.Station.objects.get(pk=ucan_id))
+        context['station_location'] = set_as_table(models.StationLocation.objects.filter(ucan_station_id=ucan_id))
+        context['station_maintenance'] = set_as_table(models.StationMaintenance.objects.filter(ucan_station_id=ucan_id))
+        context['station_equipment'] = set_as_table(models.StationEquipment.objects.filter(ucan_station_id=ucan_id))
+        context['station_physical'] = set_as_table(models.StationPhysical.objects.filter(ucan_station_id=ucan_id))
     return render_to_response('my_meta/station_detail.html', context, context_instance=RequestContext(request))
 
 def station_tables(request):
@@ -119,11 +118,7 @@ def station_tables(request):
                     if "ucan_station_id" in [ f.name for f in instance._meta.fields ]:
                         inst_name = '%s_%d' % (name, i+1)
                         table_dir[name].append(inst_name)
-                        #FIX ME: can't get set_as table to work with instance
-                        form = set_as_form(request, name, q=instance, ucan_station_id=ucan_id)
-                        table[inst_name] = form
-                        #table[inst_name] = set_as_form2(instance)
-                        #table[inst_name] = set_as_table2(instance)
+                        table[inst_name] = set_as_form(request, name, q=instance, ucan_station_id=ucan_id)
         context['ucan_station_id'] = ucan_id
         context['table'] = table
         context['table_dir'] = dict(table_dir)
@@ -148,17 +143,17 @@ def station_maintenance(request, ucan_id):
         'ucan_id': ucan_id,
     }
     if request.POST:
-        form = StationMaintenanceForm(request.POST)
+        form = mforms.StationMaintenanceForm(request.POST)
         if form.is_valid():
             form.save()
             context['saved'] = "Information saved."
         else:
             context['saved'] = "Information NOT saved."
     else:
-        (form, ucan_found) = StationMaintenanceForm(initial={'ucan_station_id': ucan_id})
+        (form, ucan_found) = mforms.StationMaintenanceForm(initial={'ucan_station_id': ucan_id})
     context['ucan_found'] = ucan_found
     context['form'] = form
-    context['station_maintenance'] = set_as_table(StationMaintenance.objects.filter(ucan_station_id=ucan_id))
+    context['station_maintenance'] = set_as_table(models.StationMaintenance.objects.filter(ucan_station_id=ucan_id))
     return render_to_response('my_meta/station_maintenance.html', context, context_instance=RequestContext(request))
 
 #@login_required
@@ -169,15 +164,15 @@ def station_physical(request, ucan_id):
         'ucan_id': ucan_id,
     }
     if request.POST:
-        form = StationPhysicalForm(request.POST)
+        form = mforms.StationPhysicalForm(request.POST)
         if form.is_valid():
             form.save()
             context['saved'] = "Information saved."
     else:
-        form = StationPhysicalForm(initial={'ucan_station_id': ucan_id})
+        form = mforms.StationPhysicalForm(initial={'ucan_station_id': ucan_id})
 
     context['form'] = form
-    context['station_physical'] = set_as_table(StationPhysical.objects.filter(ucan_station_id=ucan_id))
+    context['station_physical'] = set_as_table(models.StationPhysical.objects.filter(ucan_station_id=ucan_id))
     return render_to_response('my_meta/station_physical.html', context, context_instance=RequestContext(request))
 
 #@login_required
@@ -188,15 +183,15 @@ def station_equipment(request, ucan_id):
         'ucan_id': ucan_id,
     }
     if request.POST:
-        form = StationEquipmentForm(request.POST)
+        form = mforms.StationEquipmentForm(request.POST)
         if form.is_valid():
             form.save()
             context['saved'] = "Information saved."
     else:
-        form = StationEquipmentForm(initial={'ucan_station_id': ucan_id})
+        form = mforms.StationEquipmentForm(initial={'ucan_station_id': ucan_id})
 
     context['form'] = form
-    context['station_equipment'] = set_as_table(StationEquipment.objects.filter(ucan_station_id=ucan_id))
+    context['station_equipment'] = set_as_table(models.StationEquipment.objects.filter(ucan_station_id=ucan_id))
     return render_to_response('my_meta/station_equipment.html', context, context_instance=RequestContext(request))
 
 #
@@ -264,7 +259,7 @@ def set_as_table2(qs):
 def set_as_form(request, tbl_name, q= None,  ucan_station_id = None):
 
     form_name = "%sForm" % tbl_name
-    form_class = getattr(my_meta.forms, form_name)
+    form_class = getattr(mforms, form_name)
     if ucan_station_id is None:
         if request.POST:
             form = form_class(request.POST)
@@ -281,47 +276,6 @@ def set_as_form(request, tbl_name, q= None,  ucan_station_id = None):
     if form.is_valid():
         form.save()
     return form
-
-def set_as_form2(qs):
-    first = True
-    rows = []
-    if hasattr(qs, '_meta'):
-        for f in qs._meta.fields:
-            rows.append("%s: <input name=\" %s \" value=\" %s \" /><br />" % ( f.name, f.name, break_text(getattr(qs, f.name))))
-    else:
-        raise
-    return "\n".join(rows)
-
-
-def set_as_dict(qs):
-    """
-    Return a query set as a dictionary of key value pairs.
-    """
-    first = True
-    dic = {}
-
-    if isinstance(qs, QuerySet):
-        for i in qs:
-            if first:
-                first = False
-                for f in i._meta.fields:
-                    dic[f.name] = getattr(i, f.name)
-    elif hasattr(qs, '_meta'):
-        for f in qs._meta.fields:
-            dic[f.name] = getattr(qs, f.name)
-    else:
-        raise
-    return dic
-
-def get_keys(q):
-    """
-    Return the keys of a query as list
-    """
-    L = []
-    if hasattr(q, '_meta'):
-       for f in q._meta.fields:
-           L.append(f.name)
-    return L
 
 def break_text(s,num=60,sep="<br />"):
     """ Return string s with separator every num words."""
